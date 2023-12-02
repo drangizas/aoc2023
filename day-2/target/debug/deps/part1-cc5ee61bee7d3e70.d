cfd90ac609a4124decc55ab1570d9b2a63844d09
Zydrunas <dranginis.z@gmail.com>
2023-12-02 16:32:04 +0200

    Git cleanup

--- day-2/target/debug/deps/part1-cc5ee61bee7d3e70.d
+++
@@ -1,5 +0,0 @@
-/home/asm/projects/aoc2023/day-2/target/debug/deps/part1-cc5ee61bee7d3e70: src/bin/part1.rs
-
-/home/asm/projects/aoc2023/day-2/target/debug/deps/part1-cc5ee61bee7d3e70.d: src/bin/part1.rs
-
-src/bin/part1.rs:
--- day-2/target/debug/part1.d
+++
@@ -1,1 +0,0 @@
-/home/asm/projects/aoc2023/day-2/target/debug/part1: /home/asm/projects/aoc2023/day-2/src/bin/part1.rs
